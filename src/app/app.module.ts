1b6b888de53382b18d74ad294413c9b96f257e3e
whutchtwo <whutchinson@javaplusconsulting.com>
2022-05-14 07:21:41 -0400

    H-0000 Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ButtonsModule} from "@cleargov/buttons";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
