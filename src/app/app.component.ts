d0e4cf120fd5ca7a6050dee7df3acdf38e482791
Walter Hutchinson <whutchinson@Walters-MacBook-Air.local>
2022-05-25 13:49:01 -0400

    H-000 Update to the latest libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public count = 0;
   title = 'shared-component-app';
+
+  onClick(): void {
+    this.count++;
+  }
 }
