80badd3a5a6f35353180426e486007e2cc5765e4
Walter hutchinson <whutchinson@cleargov.com>
2022-06-13 14:25:13 -0400

    update styles and font awesome depenency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ButtonsModule} from "@cleargov/buttons";
+import {ButtonsModule} from "@cleargov/shared-ui/buttons";
 
 @NgModule({
   declarations: [
